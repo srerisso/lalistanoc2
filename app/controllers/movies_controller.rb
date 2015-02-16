debbbea814501c107edf6c3e2476e0dadc5fe409
Jose Manuel Gomez <jmgomezr@gmail.com>
2015-02-16 15:56:15 +0100

    Added paperclip for upload images

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -72,6 +72,6 @@ class MoviesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def movie_params
-      params.require(:movie).permit(:title, :description, :movie_length, :director, :rating, :productora)
+      params.require(:movie).permit(:title, :description, :movie_length, :director, :rating, :productora, :image)
     end
 end
