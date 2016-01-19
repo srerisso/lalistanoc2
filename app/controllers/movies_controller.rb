6921055ee5d6409dd5bb750f3681a9cb917fa1a1
springkid <jmgomezr@gmail.com>
2016-01-19 15:39:03 +0000

    Pelis de 2016 introducidas, cambios en listado de pelis

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,7 +5,7 @@ class MoviesController < ApplicationController
   # GET /movies
   # GET /movies.json
   def index
-    @movies = Movie.all
+    @movies = Movie.all.order("fechaestreno ASC")
   end
 
   # GET /movies/1
